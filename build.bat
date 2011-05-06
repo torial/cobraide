ae8b16d03e71c0e02da8a82d733d489a777a018c
seanm <torial@gmail.com>
2011-05-05 22:41:34 -0500

    added logic that generates a "build.bat" file so that the files can be built outside of the IDE. added the "build.bat" file for Naja.

---
+++ build.bat
@@ -0,0 +1,1 @@
+C:\Cobra\bin\cobra.bat  -d  -t:winexe CodeFolding.cobra FindAndReplace.cobra Naja.cobra Naja.WinForms.cobra Solution.cobra Tasks.cobra Utils.cobra -ref:mscorlib.dll -ref:System.dll -ref:Cobra.Lang.dll -ref:Cobra.Lang.Compiler.dll -ref:DesignSurfaceExt2.dll -ref:DesignSurfaceManagerExt.dll -ref:fastJSON.dll -ref:ICSharpCode.TextEditor.dll -ref:Qios.DevSuite.Components.dll
